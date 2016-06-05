c1edc57e3cab20d1cf384f1160f57c22959351fb
Thangiee <Thangiee0@gmail.com>
2016-06-05 11:17:03 -0500

    Add validate function for riot api key

--- src/main/scala/domain/model/Key.scala
+++ src/main/scala/domain/model/Key.scala
@@ -4,3 +4,21 @@ import domain._
 
 case class Key(value: String) extends AnyVal
 
+object Key {
+
+  import domain.io.ops._
+
+  type ValidKeys = Vector[Key]
+  type InvalidKeys = Vector[Key]
+
+  def validate(keys: Vector[Key]): IO[(ValidKeys, InvalidKeys)] = {
+    val urls = keys.map(key => s"https://na.api.pvp.net/api/lol/static-data/na/v1.2/summoner-spell/1?api_key=${key.value}")
+    mkRequests(urls)
+      .map(reqs => reqs.zip(keys))
+      .map(_.partition(isValidResp))
+      .map{ case (validResp, invalidResp) => (extractKeys(validResp), extractKeys(invalidResp))}
+  }
+
+  private def isValidResp(arg: (Response, Key)): Boolean = arg._1.is2xx
+  private def extractKeys(arg: Vector[(Response, Key)]): Vector[Key] = arg.map(_._2)
+}
